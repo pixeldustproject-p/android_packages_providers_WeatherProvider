3dce8d6f17bdea6cb1b7d14db05e53ba3b52ee2a
Chris <chris@cypheros.co>
2018-11-10 00:37:39 -0500

    WeatherProvider: Set weather constants as a public class
    Classes outside of /utils need to access this

--- src/java/com/android/providers/weather/utils/Constants.java
+++ src/java/com/android/providers/weather/utils/Constants.java
@@ -15,7 +15,7 @@
  */
 package com.android.providers.weather.utils;
 
-class Constants {
+public class Constants {
     static final Boolean DEBUG = false;
     static final int LOCATION_QUERY_MAX_TIME = 15000; // 15 seconds on milliseconds
     static final int API_CACHE_NO_CONNECTION_MAX_TIME = 3600; // 1 hour on seconds if no internet available
